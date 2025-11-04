a5ccde1e57526d822c88cbed32f6931c2671a628
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-04 23:10:22 +0100

    Create ThemeProvider.jsx

---
+++ src/theme/ThemeProvider.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+import "../theme/theme.css";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) =>
+      prev === "dark" ? "light" : prev === "light" ? "glass" : "dark"
+    );
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
