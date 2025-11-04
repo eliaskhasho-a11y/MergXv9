0710ea7c758ad64a0982c4bf7e68e45bd9ae5abb
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-04 23:07:23 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-// src/main.jsx
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./index.css"; // global styling
 
 const rootEl = document.getElementById("root");
+
 createRoot(rootEl).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
