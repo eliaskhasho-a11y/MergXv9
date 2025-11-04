39870ef0fa40ca882bac78c150ada939ad0756b0
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-04 22:43:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // projektets rot
+  root: ".", // Projektets rot
+  base: "./", // <- Denna rad löser din path-bugg på Vercel
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   build: {
     outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, "index.html"),
+      input: "index.html",
     },
   },
   server: {
