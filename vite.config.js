8a4cf3039fa11eda29f75b0794e24f5df2af4410
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-04 22:42:02 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // gör att index.html hittas korrekt av Vercel
+  root: ".", // projektets rot
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "src"),
     },
   },
   build: {
     outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
+      input: resolve(__dirname, "index.html"),
     },
   },
   server: {
     port: 5173,
     open: true,
     historyApiFallback: true,
-  },
-  preview: {
-    port: 4173,
-    open: true,
   },
 });
