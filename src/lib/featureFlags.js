5a8460e6a7bc36dc7aea669a734a4eee26679820
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-04 22:17:01 +0100

    Create featureFlags.js

---
+++ src/lib/featureFlags.js
@@ -0,0 +1,10 @@
+// Feature flags för trygg aktivering/deaktivering på UI-nivå.
+export const flags = {
+  aiCoach: true,
+  b2bIntegration: true,
+  whatIfStudio: true,
+  connectorSdk: true,
+  auditLog: true,
+  aiQualityControl: true,
+  meetingMemory: true,
+};
