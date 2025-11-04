4a55a0f6792c96f4b38684197a26d166b45285ce
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-04 23:20:31 +0100

    Update Ekonomi.jsx

--- src/pages/Ekonomi.jsx
+++ src/pages/Ekonomi.jsx
@@ -9,13 +9,3 @@ export default function Ekonomi() {
     </div>
   );
 }
-
-import EconomicChart from "../components/charts/EconomicChart";
-
-export default function Ekonomi() {
-  return (
-    <div className="page ekonomi-page">
-      <EconomicChart />
-    </div>
-  );
-}
