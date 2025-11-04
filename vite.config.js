6b9366b42023e0f5f9c59d94ddb95cf0b738432d
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-04 22:28:45 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+    historyApiFallback: true, // gör att /dashboard osv. fungerar
+  },
+});
