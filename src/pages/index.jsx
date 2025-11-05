6a4624d173a7d8b643ca6e621d617e1376dec966
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-05 15:20:18 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,33 +1,44 @@
-export { default as AiMap } from "./AiMap";
-export { default as AiSalesMap } from "./AiSalesMap";
-export { default as AdminSettings } from "./AdminSettings";
-export { default as AiAccessManager } from "./AiAccessManager";
-export { default as AiAnalys } from "./AiAnalys";
-export { default as AiAuditAnalytics } from "./AiAuditAnalytics";
-export { default as AiAuthGateway } from "./AiAuthGateway";
-export { default as AiAutoResponseEngine } from "./AiAutoResponseEngine";
-export { default as AiCloudRealtimeEngine } from "./AiCloudRealtimeEngine";
-export { default as AiCloudSyncHub } from "./AiCloudSynchHub";
-export { default as AiCommandCenter } from "./AiCommandCenter";
-export { default as AiCommandConsole } from "./AiCommandConsole";
-export { default as AiMaintenanceScheduler } from "./AiMaintenanceScheduler";
-export { default as AiPerformanceDashboard } from "./AiPerformanceDashboard";
-export { default as AiRuntimeConfigCenter } from "./AiRuntimeConfigCenter";
-export { default as AiSecurityCenter } from "./AiSecurityCenter";
-export { default as AiSessionManager } from "./AiSessionManager";
-export { default as AiSystemControlPanel } from "./AiSystemControlPanel";
-export { default as AiSystemHealthMonitor } from "./AiSystemHealthMonitor";
-export { default as CRM } from "./CRM";
-export { default as Dashboard } from "./Dashboard";
-export { default as DashboardOverview } from "./DashboardOverview";
-export { default as Economy } from "./Economy";
-export { default as EconomyDashboard } from "./EconomyDashboard";
-export { default as Ekonomi } from "./Ekonomi";
-export { default as EnergyEfficiencyDashboard } from "./EnergyEfficiencyDashboard";
-export { default as Inventory } from "./Inventory";
-export { default as KpiPanel } from "./KpiPanel";
-export { default as Lager } from "./Lager";
-export { default as MergxOpsCloudPanel } from "./MergxOpsCloudPanel";
-export { default as TeamChat } from "./TeamChat";
-export { default as ThreatAiClassifier } from "./ThreatAiClassifier";
-export { default as UserDirectory } from "./UserDirectory";
+// Samlad export av alla page-komponenter (case-korrekt för Vercel)
+
+export { default as AiMap } from "./AiMap.jsx";
+export { default as AiSalesMap } from "./AiSalesMap.jsx";
+
+export { default as AdminSettings } from "./AdminSettings.jsx";
+
+export { default as AiAccessManager } from "./AiAccessManager.jsx";
+export { default as AiAnalys } from "./AiAnalys.jsx";
+export { default as AiAuditAnalytics } from "./AiAuditAnalytics.jsx";
+export { default as AiAuthGateway } from "./AiAuthGateway.jsx";
+export { default as AiAutoResponseEngine } from "./AiAutoResponseEngine.jsx";
+
+export { default as AiCloudRealtimeEngine } from "./AiCloudRealtimeEngine.jsx";
+export { default as AiCloudSynchHub } from "./AiCloudSynchHub.jsx"; // OBS: “Synch” enligt filnamn i repo
+
+export { default as AiCommandCenter } from "./AiCommandCenter.jsx";
+export { default as AiCommandConsole } from "./AiCommandConsole.jsx";
+export { default as AiMaintenanceScheduler } from "./AiMaintenanceScheduler.jsx";
+export { default as AiPerformanceDashboard } from "./AiPerformanceDashboard.jsx";
+export { default as AiRuntimeConfigCenter } from "./AiRuntimeConfigCenter.jsx";
+export { default as AiSecurityCenter } from "./AiSecurityCenter.jsx";
+export { default as AiSessionManager } from "./AiSessionManager.jsx";
+export { default as AiSystemControlPanel } from "./AiSystemControlPanel.jsx";
+export { default as AiSystemHealthMonitor } from "./AiSystemHealthMonitor.jsx";
+
+export { default as CRM } from "./CRM.jsx";
+export { default as Dashboard } from "./Dashboard.jsx";
+export { default as DashboardOverview } from "./DashboardOverview.jsx";
+
+export { default as Economy } from "./Economy.jsx";
+export { default as EconomyDashboard } from "./EconomyDashboard.jsx";
+export { default as Ekonomi } from "./Ekonomi.jsx";
+export { default as EnergyEfficiencyDashboard } from "./EnergyEfficiencyDashboard.jsx";
+
+export { default as Inventory } from "./Inventory.jsx";
+export { default as KpiPanel } from "./KpiPanel.jsx";
+export { default as Lager } from "./Lager.jsx";
+
+export { default as MergxOpsCloudPanel } from "./MergxOpsCloudPanel.jsx";
+
+export { default as TeamChat } from "./TeamChat.jsx";
+export { default as ThreatAiClassifier } from "./ThreatAiClassifier.jsx";
+export { default as UserDirectory } from "./UserDirectory.jsx";
