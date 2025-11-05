3bc0b34558709020518040f0fef29970290b866b
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-05 14:24:49 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,42 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2021: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:prettier/recommended"
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "detect" } },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: "detect" },
+  },
   plugins: ["react", "react-hooks", "jsx-a11y", "import", "prettier"],
   rules: {
     "prettier/prettier": ["error"],
     "react/prop-types": "off",
     "import/no-unresolved": "error",
     "react/react-in-jsx-scope": "off",
-    // stoppar case-fel på imports
     "import/no-absolute-path": "error",
+    "import/no-useless-path-segments": "error",
+    "import/no-duplicates": "error",
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal"],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
   },
 };
