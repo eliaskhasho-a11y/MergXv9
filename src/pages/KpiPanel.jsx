85a24e183bd87942abb816f0133400c23e9cc39b
eliaskhasho-a11y <elias.khasho@gmail.com>
2025-11-05 13:10:07 +0100

    Create KpiPanel.jsx

---
+++ src/pages/KpiPanel.jsx
@@ -0,0 +1,13 @@
+// src/pages/KpiPanel.jsx
+import React from "react";
+
+const KpiPanel = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>KPI-Panel</h1>
+      <p>Här kommer KPI-modulen att visas.</p>
+    </div>
+  );
+};
+
+export default KpiPanel;
